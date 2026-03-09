4d4ff9a9ca2d01b61a329fee28cb5447bf815c78
G. Sri Lasya <srilasyagandrala11@gmail.com>
2026-03-09 17:50:55 +0530

    Obscure database password in settings.py
    Updated database password to be obscured.

--- personal_manager/settings.py
+++ personal_manager/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "personal_manager_db",
         "USER": "postgres",
-        "PASSWORD": "lasya1234",
+        "PASSWORD": "****",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
