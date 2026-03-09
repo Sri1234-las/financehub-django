96ce9e4197656976a61649dd6623c3102834f270
G. Sri Lasya <srilasyagandrala11@gmail.com>
2026-03-09 17:53:39 +0530

    Change PostgreSQL password in settings
    Updated database password for PostgreSQL connection.

--- personal_manager/settings.py
+++ personal_manager/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "personal_manager_db",
         "USER": "postgres",
-        "PASSWORD": "*****",
+        "PASSWORD": "lasya1234",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
